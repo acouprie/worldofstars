962c432a17a482a619b0ca9724e3802dd8a1f9aa
acouprie <acouprie@hotmail.fr>
2017-01-20 11:49:43 +0100

    register function quite complete

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
 	get '/register', to: 'users#new'
   	get '/contact', to: 'static_pages#contact'
+  	resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
