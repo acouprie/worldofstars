71bab36a745bed943c982b9235601a43a4a45334
acouprie <acouprie@hotmail.fr>
2017-01-20 20:51:31 +0100

    improve frontend color

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 Rails.application.routes.draw do
 	
+  get 'sessions/new'
+
   get 'users/new'
-
+  	# URL home page
 	root 'static_pages#home'
-
+	# URL signup
 	get '/signup', to: 'users#new'
 	post '/signup', to: 'users#create'
+	# URL contact information
   	get '/contact', to: 'static_pages#contact'
+  	# URL sessions
+  	get '/login', to: 'sessions#new'
+	post '/login',to: 'sessions#create'
+	delete '/logout', to: 'sessions#destroy'
+	
   	resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
