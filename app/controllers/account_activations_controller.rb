6014e397f5582b585d85bd8e0f6868f8ac79e9e9
acouprie <acouprie@hotmail.fr>
2017-01-22 15:23:54 +0100

    Send email verification RED pagination problem ?

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -2,14 +2,13 @@ class AccountActivationsController < ApplicationController
 	def edit
 		user = User.find_by(email: params[:email])
 		if user && !user.activated? && user.authenticated?(:activation, params[:id])
-			user.update_attribute(:activated,
-			true)
+			user.update_attribute(:activated, true)
 			user.update_attribute(:activated_at, Time.zone.now)
 			log_in user
-			flash[:success] = "Account activated!"
+			flash[:success] = "Compte activé, l'aventure commence !"
 			redirect_to user
 		else
-			flash[:danger] = "Invalid activation link"
+			flash[:danger] = "Lien d'activation incorrect, contactez l'administrateur"
 			redirect_to root_url
 		end
 	end
--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -3,13 +3,14 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
     @admin     = users(:Frodo)
     @non_admin = users(:ObiWan)
+    @non_activated = users(:JohnSnow)
   end
 
   test "index as admin including pagination and delete links" do
     log_in_as(@admin)
     get users_path
     assert_template 'users/index'
-    # assert_select 'div.pagination'
+    assert_select 'div.pagination'
     first_page_of_users = User.paginate(page: 1)
     first_page_of_users.each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
@@ -21,9 +22,16 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
       delete user_path(@non_admin)
     end
   end
+
   test "index as non-admin" do
     log_in_as(@non_admin)
     get users_path
     assert_select 'a', text: 'delete', count: 0
+  end
+
+  test "should not display unactivated account" do
+      log_in_as(@non_admin)
+      get users_path
+      assert_select 'a', text: @non_activated.name, count: 0
   end
 end
