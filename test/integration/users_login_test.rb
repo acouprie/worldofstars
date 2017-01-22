a6a16836afd5a595c20d6e6462fd22ba21cc6541
acouprie <acouprie@hotmail.fr>
2017-01-22 01:21:30 +0100

    Improve integration tests GREEN

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -21,9 +21,17 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 	    assert_redirected_to @user
 	    follow_redirect!
 	    assert_template 'users/show'
-	    assert_select "a[href=?]", login_path, count: 0
-	    assert_select "a[href=?]", logout_path
+
+	    assert_select "a[href=?]", root_path
+	    assert_select "a[href=?]", users_path
+	    assert_select "a[href=?]", user_path
+	   	assert_select "a[href=?]", contact_path
 	    assert_select "a[href=?]", user_path(@user)
+	    assert_select "a[href=?]", logout_path
+
+	    assert_select "a[href=?]", login_path, count: 0
+	    assert_select "a[href=?]", signup_path, count: 0
+
 	    delete logout_path
 	    assert_not is_logged_in?
 	    assert_redirected_to root_url
@@ -31,8 +39,13 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 	    delete logout_path
 	    follow_redirect!
 	    assert_select "a[href=?]", login_path
-	    assert_select "a[href=?]", logout_path,      count: 0
+	    assert_select "a[href=?]", signup_path
+	   	assert_select "a[href=?]", contact_path
+
+	    assert_select "a[href=?]", users_path, count: 0
 	    assert_select "a[href=?]", user_path(@user), count: 0
+	    assert_select "a[href=?]", logout_path,      count: 0
+
   	end
 
   test "login with remembering" do
