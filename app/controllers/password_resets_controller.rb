eba9e96a350b84781ec135670e5518cee8c13f20
acouprie <acouprie@hotmail.fr>
2017-01-22 17:39:04 +0100

    Implement password reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,9 @@
 class PasswordResetsController < ApplicationController
+
+	before_action :get_user, only: [:edit, :update]
+	before_action :valid_user, only: [:edit, :update]
+	before_action :check_expiration, only: [:edit, :update]
+
   def new
   end
 
@@ -15,6 +20,48 @@ class PasswordResetsController < ApplicationController
 	end
   end
 
+  	def update
+		if params[:user][:password].empty?
+			@user.errors.add(:password, "ne peut être vide")
+			render 'edit'
+		elsif @user.update_attributes(user_params)
+			log_in @user
+			@user.update_attribute(:reset_digest, nil)
+			flash[:success] = "Mot de passe réinitialiser."
+			redirect_to @user
+		else
+			render 'edit'
+		end
+	end
+
   def edit
   end
+
+  private
+
+  	def user_params
+		params.require(:user).permit(:password, :password_confirmation)
+	end
+
+	# Before filters
+
+	def get_user
+		@user = User.find_by(email: params[:email])
+	end
+
+	# Confirms a valid user.
+	def valid_user
+		unless (@user && @user.activated? &&
+			@user.authenticated?(:reset, params[:id]))
+			redirect_to root_url
+		end
+	end
+
+	# Checks expiration of reset token.
+	def check_expiration
+		if @user.password_reset_expired?
+			flash[:danger] = "Le lien a expiré."
+			redirect_to new_password_reset_url
+		end
+	end
 end
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -2,17 +2,26 @@ require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
   test "account_activation" do
-    user = users(:Frodo)
+    user = users(:ObiWan)
     user.activation_token = User.new_token
     mail = UserMailer.account_activation(user)
     assert_equal "Activation du compte", mail.subject
     assert_equal [user.email], mail.to
     assert_equal ["noreply@example.com"], mail.from
-    assert_match user.name,
-    mail.body.encoded
-    assert_match user.activation_token,
-    mail.body.encoded
+    assert_match user.name, mail.body.encoded
+    assert_match user.activation_token, mail.body.encoded
     assert_match CGI.escape(user.email), mail.body.encoded
   end
 
+    test "password_reset" do
+        user = users(:ObiWan)
+        user.reset_token = User.new_token
+        mail = UserMailer.password_reset(user)
+        assert_equal "Mot de passe", mail.subject
+        assert_equal [user.email], mail.to
+        assert_equal ["noreply@example.com"], mail.from
+        assert_match user.reset_token,
+        mail.body.encoded
+        assert_match CGI.escape(user.email), mail.body.encoded
+    end
 end
