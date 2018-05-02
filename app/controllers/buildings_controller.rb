8bfb2df6eec461d82b3553dff59d342d9acd2b2b
acouprie <acouprie@hotmail.fr>
2018-05-03 00:12:26 +0200

    Add building farm test

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,6 +1,5 @@
 class BuildingsController < ApplicationController
   before_action :set_building, only: [:show, :update, :destroy]
-
   # GET /buildings/1
   # GET /buildings/1.json
   def show
@@ -16,5 +15,15 @@ class BuildingsController < ApplicationController
   # Never trust parameters from the scary internet, only allow the white list through.
   def building_params
     params.require(:building).permit(:name, :planet_id, :food_price, :metal_price, :thorium_price, :lvl, :conso_power, :time_to_build, :production, :position)
+  end
+
+  # Confirms a logged-in user.
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Veuillez vous connecter pour effectuer cette action."
+      redirect_to login_url
+    end
   end
 end
--- test/controllers/buildings_controller_test.rb
+++ test/controllers/buildings_controller_test.rb
@@ -2,6 +2,14 @@ require 'test_helper'
 
 class BuildingsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @building = buildings(:one)
+    @user = users(:Frodo)
+    @planet = planets(:Terre)
+    @champs = buildings(:champs)
+  end
+
+  test "should get new farm" do
+    assert_difference('Building.count') do
+      @user.planets.first.create_farm
+    end
   end
 end
